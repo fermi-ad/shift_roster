caf0fc43547c2b51897d2a903a22174a6634e3b0
Beau Harrison <beauharrison@gmail.com>
2016-05-01 06:10:15 -0500

    Successfully POSTing static text to newest 'Shift Change' note's entry ID

--- js/main.js
+++ js/main.js
@@ -1,29 +1,42 @@
 'use strict';
 
-function getEntryID() {
-    var entryID;
-    $.ajax({
-            url: 'https://www-bd.fnal.gov/Elog/?categoryNames=Shift+Change&limit=1',
-            type: 'GET',
-            async: false
-        })
-        .done(function(data) {
-            entryID = parseForEntryID(data);
-            console.log("success");
-        })
-        .fail(function() {
-            console.log("error");
-        })
-        .always(function() {
-            console.log("complete");
-        });
-
-    return entryID;
-}
-
 function parseForEntryID(htmlStr) {
     var el = $('<div></div>');
     el.html(htmlStr);
 
     return $('.id a', el).text(); // All the class="id" elements
+}
+
+function rosterPost(id) {
+    var entryText = "<u><strong>Owl+ Shift Roster</strong></u><ul><li>Michael Wren <strong>CC</strong></li><li>Kelli Rubrecht</li><li>Michael Olander</li><li>John Kolpin</li><li>Beau Harrison</li></ul>";
+    var formData = new FormData();
+    formData.append('entryID', id);
+    formData.append('text', entryText);
+
+    $.ajax({
+            url: 'https://www-bd.fnal.gov/Elog/addComment',
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false
+        })
+        .done(function() {
+            console.log("POST success");
+        })
+        .fail(function() {
+            console.log("POST error");
+        })
+        .always(function() {
+            console.log("POST complete");
+        });
+
+}
+
+function makePost() {
+    $.when(
+            $.ajax('https://www-bd.fnal.gov/Elog/?categoryNames=Shift+Change&limit=1'))
+        .done(function(html) {
+            var entryID = parseForEntryID(html);
+            rosterPost(entryID);
+        });
 }
